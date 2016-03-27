5496dcbb7c61b6d03355d446d0f484d7a236839e
Ivan Nebritov <i.nebritov@gmail.com>
2016-03-27 14:54:45 +0300

    Configured Grappelli admin interface.
    Signed-off-by: Ivan Nebritov <i.nebritov@gmail.com>

--- world_countries/settings.py
+++ world_countries/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Grappelli admin interface settings
+
+GRAPPELLI_ADMIN_TITLE = 'World countries'
