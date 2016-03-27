ca7cbcc4561a4f376b3378475527ad7e8e92b007
Ivan Nebritov <i.nebritov@gmail.com>
2016-03-27 18:24:07 +0300

    Added Country model.
    Signed-off-by: Ivan Nebritov <i.nebritov@gmail.com>

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,12 @@
-from core.models import Language
+from core.models import Language, Country
 from django.contrib import admin
 
 
 @admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
     list_display = ('alpha2', 'English',)
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('alpha3', 'name', 'population',)
--- core/models.py
+++ core/models.py
@@ -10,3 +10,21 @@ class Language(models.Model):
 
     def __unicode__(self):
         return self.__str__()
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+    population = models.IntegerField()
+    alpha2 = models.CharField(max_length=2, unique=True)
+    alpha3 = models.CharField(max_length=3, unique=True)
+    languages = models.ManyToManyField(Language)
+    neighbors = models.ManyToManyField('self')
+
+    def __str__(self):
+        return "{name}({alpha3})".format(**self.__dict__)
+
+    def __unicode__(self):
+        return self.__str__()
+
+    class Meta:
+        verbose_name_plural = 'Countries'
