76a618abc658d06a08cc879cbbff0a9dbc291106
Ivan Nebritov <i.nebritov@gmail.com>
2016-03-27 16:24:25 +0300

    Added Language model.
    Signed-off-by: Ivan Nebritov <i.nebritov@gmail.com>

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Language(models.Model):
+    alpha2 = models.CharField(max_length=2, unique=True)
+    English = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "{English}({alpha2})".format(**self.__dict__)
+
+    def __unicode__(self):
+        return self.__str__()
