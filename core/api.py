4bfde38d59a50bfb15844d3624438fc3a4660252
Ivan Nebritov <i.nebritov@gmail.com>
2016-03-27 21:23:45 +0300

    Improved neighbor field serialization.
    Signed-off-by: Ivan Nebritov <i.nebritov@gmail.com>

--- core/api.py
+++ core/api.py
@@ -4,5 +4,5 @@ from core import views
 
 urlpatterns = [
     url(r'countries/$', views.CountryList.as_view()),
-    url(r'countries/(?P<pk>[0-9]+)/$', views.CountryDetail.as_view()),
+    url(r'countries/(?P<pk>[0-9]+)/$', views.CountryDetail.as_view(), name='country-detail'),
 ]
--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Country
+from core.models import Country, Language
 
 
 class CountryListSerializer(serializers.ModelSerializer):
@@ -9,9 +9,17 @@ class CountryListSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'alpha3', 'population',)
 
 
+class NeighborSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='country-detail')
+
+    class Meta:
+        model = Country
+        fields = ('name', 'url')
+
+
 class CountryDetailsSerializer(serializers.ModelSerializer):
     languages = serializers.StringRelatedField(many=True)
-    neighbors = serializers.StringRelatedField(many=True)
+    neighbors = NeighborSerializer(many=True)
 
     class Meta:
         model = Country
